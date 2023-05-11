c88c8617c0986f784eb2dc8f0ebedddb13ed782e
Moimoichi <moimoi1594@gmail.com>
2023-05-12 02:54:13 +0800

    testing

--- App.tsx
+++ App.tsx
@@ -3,17 +3,21 @@ import { View, Text, StatusBar, TextInput, Button, Alert, FlatList } from "react
 import { DataSource} from "typeorm"
 import { Category } from "./entities/listitems";
 
-export const AppDataSource = new DataSource({
+const AppDataSource = new DataSource({
       type: "react-native",
       database: "./main.sqlite",
       location: "default",
       logging: ["error"],
       entities: [Category],
-      synchronize: true,
-      driver: require("typeorm-react-native-adapter").ReactNativeDriver,
 })
 
- 
+AppDataSource.initialize()
+  .then(() => {
+      console.log("Data Source has been initialized!")
+  })
+  .catch((err) => {
+      console.error("Error during Data Source initialization", err)
+  })
 
 
 const App = () => {
@@ -21,74 +25,15 @@ const App = () => {
   const [categories, setCategories] = useState<{ id: any; name: any; }[]>([]);
 
 
-  useEffect(() => {
-    async function setup() {
-      await createTables();
-      await getCategories();
-    }
-    setup();
-  }, []);
-  
+  const addCategory = await DataSource
+    .createQueryBuilder()
+    .select("user")
+    .from(User, "user")
+    .where("user.id = :id", { id: 1 })
+    .getOne()
+}
+ 
 
-  const createTables = async () => {
-    (await db).transaction((txn: { executeSql: (arg0: string, arg1: never[], arg2: () => void, arg3: (error: any) => void) => void; }) => {
-      txn.executeSql(
-        "CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) NOT NULL)",
-        [],
-        () => {
-          console.log("Table created Succesfully");
-        },
-        (        error: string) => {
-          console.log("Error on creating table" + error);
-        }
-      );
-    });
-  };
-
-  const addCategory = async () => {
-    if(!category){
-      Alert.alert("Enter Category");
-      return false;
-    }
-    (await AppDataSource).transaction (txn =>{
-      return txn.executeSql(
-        `INSERT INTO categories (name) VALUES (?)`,
-        [category],
-        () => {
-          console.log(`${category}item added successfully `);
-          getCategories();
-        },
-        (        error: string) => {
-          console.log("Error on adding item" + error);
-        }
-      );
-    })
-  };
-
-  const getCategories = async () => {
-    (await db).transaction((txn: { executeSql: (arg0: string, arg1: never[], arg2: (txn: any, res: any) => void, arg3: (error: any) => void) => void; }) => {
-      txn.executeSql(
-        `SELECT * FROM categories ORDER BY Id DESC`,
-        [],
-        (txn: any, res: { rows: { length: any; item: (arg0: number) => any; }; }) => {
-          console.log("Successfully retrieved ITEM");
-          let len = res.rows.length;
-  
-          if (len > 0) {
-            let results = [];
-            for (let i = 0; i < len; i++) {
-              let item = res.rows.item(i);
-              results.push({ id: item.id, name: item.name });
-            }
-            setCategories(results);
-          }
-        },
-        (error: string) => {
-          console.log("Error on getting ITEMS" + error);
-        }
-      );
-    });
-  };
   
 const renderCategory = ({ item }: { item: { id: number; name: string } }) => {
 
@@ -118,3 +63,7 @@ const renderCategory = ({ item }: { item: { id: number; name: string } }) => {
 
 
 export default App;
+function createTables() {
+  throw new Error("Function not implemented.");
+}
+
--- entities/listitems.ts
+++ entities/listitems.ts
@@ -3,7 +3,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 @Entity()
 export class Category {
   @PrimaryGeneratedColumn()
-  id!: number;
+  id!: string;
 
   @Column()
   name!: string;
